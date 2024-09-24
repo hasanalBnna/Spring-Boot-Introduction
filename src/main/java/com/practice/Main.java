4d7ceb8952af8b9863886de617a154ade6d85159
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-09-24 21:25:19 +0600

    2nd commit

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -38,6 +40,19 @@ public class Main {
             return "GreetResponse{" +
                     "greeting='" + greeting + '\'' +
                     '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greeting, that.greeting);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(greeting);
         }
     }
 
